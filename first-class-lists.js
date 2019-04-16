0d78b3114d03dc6af22dbc9a3283548bf9580625
rdococ <rd606six@googlemail.com>
2019-04-16 14:44:37 +0100

    AAAAA

--- first-class-lists.js
+++ first-class-lists.js
@@ -1,25 +1,31 @@
-var FirstClassLists = function (runtime) {
-	this.runtime = runtime
-};
+class Test {
 
-FirstClassLists.prototype.getInfo = function () {
-	return {
-		id: "FirstClassLists",
-		name: "First Class Lists",
-		
-		blocks = [
-			{
-				opcode: "test",
-				blockType: BlockType.REPORTER,
-				text: "test",
-				arguments: []
-			}
-		]
-	}
+  constructor() {
+    
+  }
+
+  getInfo() {
+    return {
+      id: 'test',
+      name: 'Test',
+
+      colour: '#ef5350',
+      colourSecondary: '#f44336',
+      colourTertiary: '#e53935',
+
+      menuIconURI: icon,
+      blockIconURI: icon,
+
+      blocks: [
+        {
+          opcode: 'test',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'test',
+          arguments: {}
+        }
+      ]
+    }
+  }
 }
 
-FirstClassLists.prototype.test = function () {
-	return prompt("Prompt from extension:");
-};
-
-Scratch.extensions.register(new FirstClassLists());
+Scratch.extensions.register(new Test());
